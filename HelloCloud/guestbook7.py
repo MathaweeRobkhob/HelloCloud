7d3c674b868c7ed96294a7ad8ed0af673177450d
MathaweeRobkhob <6406022620061@fitm.kmutnb.ac.th>
2022-10-04 18:24:08 +0700

    up

--- HelloCloud/guestbook7.py
+++ HelloCloud/guestbook7.py
@@ -33,4 +33,4 @@ def process():
     return redirect(url_for('index'))
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", post=80,debug=True)
+    app.run(host="0.0.0.0", port=80,debug=True)
