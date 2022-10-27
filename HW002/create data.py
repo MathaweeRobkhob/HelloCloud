bc0490f1afd626f81a67b3c51372da999a255b41
MathaweeRobkhob <6406022620061@fitm.kmutnb.ac.th>
2022-10-27 23:14:51 +0700

    u

---
+++ HW002/create data.py
@@ -0,0 +1,28 @@
+import psycopg2
+try:
+    connection = psycopg2.connect(user='webadmin',
+                                    password='XBDqhh16389',
+                                    host='node37005-mathawee.proen.app.ruk-com.cloud',
+                                    port='5432',
+                                    database='postgres')
+
+    connection.autocommit = True
+
+    #Creating a cursor object using the cursor() method
+    cursor = connection.cursor()
+
+    # Preparing query to create a database
+    sql = '''CREATE database work'''
+
+    #Creating  a database
+    cursor.execute(sql)
+    print('Database created sucessfully.........')
+
+except (Exception,psycopg2.Error) as error:
+    print('Error while connecting to PostgreSQL',error)
+finally:
+    # closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print('PostgreSQL connection is closed')
